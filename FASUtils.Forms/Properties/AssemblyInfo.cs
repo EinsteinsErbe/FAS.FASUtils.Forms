a616c93d5b39bf74114e90e6979d7628123484f4
Manuel Jordan <manuel.jordan@ch.abb.com>
2019-12-09 11:05:49 +0100

    change assemby info

--- FASUtils.Forms/Properties/AssemblyInfo.cs
+++ FASUtils.Forms/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FASUtils.Forms")]
+[assembly: AssemblyTitle("FAS.FASUtils.Forms")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("A collection of Windows Forms functions for the FAS team ")]
+[assembly: AssemblyCompany("Manuel Jordan")]
 [assembly: AssemblyProduct("FASUtils.Forms")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
