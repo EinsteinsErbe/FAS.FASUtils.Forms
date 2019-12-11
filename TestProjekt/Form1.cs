556c032accce6779a48a5782825eaf383c9f0e1d
Manuel Jordan <manuel.jordan@ch.abb.com>
2019-12-11 10:10:40 +0100

    add test project

---
+++ TestProjekt/Form1.cs
@@ -0,0 +1,32 @@
+﻿using FASUtils;
+using FASUtils.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestProjekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Connectable.INTERNET.AddControl(label1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Connectable.INTERNET.Check();
+        }
+    }
+}
--- TestProjekt/Properties/AssemblyInfo.cs
+++ TestProjekt/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FASUtils.Forms")]
-[assembly: AssemblyDescription("A collection of Windows Forms functions for the FAS team.")]
+[assembly: AssemblyTitle("TestProjekt")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Manuel Jordan")]
-[assembly: AssemblyProduct("FASUtils.Forms")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProjekt")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7b1c397e-39cb-44f4-ba59-9a67a9cf87db")]
+[assembly: Guid("e065324a-c4f2-47b2-9da9-2809304e6584")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
