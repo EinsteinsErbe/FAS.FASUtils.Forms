4d2aaaf2b83dbfd12bea9339d4e1fd5c2e6d584d
Manuel Jordan <manuel.jordan@ch.abb.com>
2019-12-12 16:27:20 +0100

    add TreeView, add FolderBrowserDialog

---
+++ FASUtils.Forms/TreeViewV2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MetroFramework.Controls
+{
+    public partial class TreeViewV2 : TreeView
+    {
+        public TreeViewV2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // Suppress WM_LBUTTONDBLCLK
+            if (m.Msg == 0x203) { m.Result = IntPtr.Zero; }
+            else base.WndProc(ref m);
+        }
+    }
+}
